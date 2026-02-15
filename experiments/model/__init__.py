7cf217211cd3a3dce80853f983e2aee910e7492a
fei1lee <951414390@qq.com>
2026-02-15 21:23:33 +0800

    remove __pycache__

--- experiments/model/__init__.py
+++
@@ -1,9 +0,0 @@
-from config.structured import MultiViewConfig
-
-from .model_gen import MultiViewPointCloudDiffusionModel
-
-
-def get_model(cfg: MultiViewConfig):
-    multi_view_model = MultiViewPointCloudDiffusionModel(**cfg.model)
-    return multi_view_model
-
